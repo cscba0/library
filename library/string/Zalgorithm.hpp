effd4c5ff555d7ae7e3b07b1a52e56fd28172989
cscba0 <cscba01367@gmail.com>
2025-04-20 16:34:15 +0900

    update: compile options

--- library/string/Zalgorithm.hpp
+++ library/string/Zalgorithm.hpp
@@ -7,14 +7,14 @@ inline std::vector<int> Zalgorithm(const std::string& s) noexcept {
     std::vector<int> Z(s.size());
     Z[0] = s.size();
     for (uint32_t i = 1, j = 0, siz = s.size(); i < siz;) {
-        while (i + j < s.size() && s[j] == s[i + j]) {
+        while (i + j < siz && s[j] == s[i + j]) {
             ++j;
         }
         Z[i] = j;
         if (j == 0) {
             ++i;
         } else {
-            uint k = 1;
+            uint32_t k = 1;
             for (; k < j && k + Z[k] < j; ++k) {
                 Z[i + k] = Z[k];
             }
