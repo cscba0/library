f6ed15f237c1872f6e279f6b8378f3d207be9961
cscba0 <cscba01367@gmail.com>
2025-04-26 19:55:12 +0900

    update: fastio

--- fastio/unsigned/read.hpp
+++ fastio/unsigned/read.hpp
@@ -6,11 +6,11 @@
 template <std::unsigned_integral T>
 inline FASTIO& operator>>(FASTIO& io, T& x) noexcept {
     io.seek();
-    std::common_type_t<T, uint64_t> y = 0;
+    uint64_t y = 0;
     {
         uint64_t v;
         memcpy(&v, io.ipos, 8);
-        if (!((v -= 0x3030303030303030) & 0x8080808080808080)) {
+        if (v ^= 0x3030303030303030, !(v & 0xf0f0f0f0f0f0f0f0)) {
             v = (v * 10 + (v >> 8)) & 0xff00ff00ff00ff;
             v = (v * 100 + (v >> 16)) & 0xffff0000ffff;
             v = (v * 10000 + (v >> 32)) & 0xffffffff;
@@ -21,7 +21,7 @@ inline FASTIO& operator>>(FASTIO& io, T& x) noexcept {
     {
         uint64_t v;
         memcpy(&v, io.ipos, 8);
-        if (!((v -= 0x3030303030303030) & 0x8080808080808080)) {
+        if (v ^= 0x3030303030303030, !(v & 0xf0f0f0f0f0f0f0f0)) {
             v = (v * 10 + (v >> 8)) & 0xff00ff00ff00ff;
             v = (v * 100 + (v >> 16)) & 0xffff0000ffff;
             v = (v * 10000 + (v >> 32)) & 0xffffffff;
@@ -32,7 +32,7 @@ inline FASTIO& operator>>(FASTIO& io, T& x) noexcept {
     {
         uint32_t v;
         memcpy(&v, io.ipos, 4);
-        if (!((v -= 0x30303030) & 0x80808080)) {
+        if (v ^= 0x30303030, !(v & 0xf0f0f0f0)) {
             v = (v * 10 + (v >> 8)) & 0xff00ff;
             v = (v * 100 + (v >> 16)) & 0xffff;
             y = 10000 * y + v;
@@ -42,7 +42,7 @@ inline FASTIO& operator>>(FASTIO& io, T& x) noexcept {
     {
         uint16_t v;
         memcpy(&v, io.ipos, 2);
-        if (!((v -= 0x3030) & 0x8080)) {
+        if (v ^= 0x3030, !(v & 0xf0f0)) {
             v = (v * 10 + (v >> 8)) & 0xff;
             y = 100 * y + v;
             io.ipos += 2;
--- fastio/unsigned/write.hpp
+++ fastio/unsigned/write.hpp
@@ -4,7 +4,7 @@
 #include <cstring>
 #include <fastio/base.hpp>
 
-static constexpr auto FastLookUpTable = [] {
+static constexpr auto FastIODigitTable = [] {
     std::array<std::array<char, 4>, 10000> res;
     for (int i = 0; i < 10000; ++i) {
         res[i][0] = '0' + i / 1000;
@@ -16,44 +16,44 @@ static constexpr auto FastLookUpTable = [] {
 }();
 
 inline void FastWrite_(FASTIO& io, uint16_t x) noexcept {
-    memcpy(io.opos, &FastLookUpTable[x], 4);
+    memcpy(io.opos, &FastIODigitTable[x], 4);
     io.opos += 4;
 }
 
 inline void FastWrite__(FASTIO& io, uint16_t x) noexcept {
     if (x > 999) {
-        memcpy(io.opos, &FastLookUpTable[x], 4);
+        memcpy(io.opos, &FastIODigitTable[x], 4);
         io.opos += 4;
     } else if (x > 99) {
-        memcpy(io.opos, &FastLookUpTable[x * 10], 3);
+        memcpy(io.opos, &FastIODigitTable[x * 10], 3);
         io.opos += 3;
     } else if (x > 9) {
-        memcpy(io.opos, &FastLookUpTable[x * 100], 2);
+        memcpy(io.opos, &FastIODigitTable[x * 100], 2);
         io.opos += 2;
     } else {
-        memcpy(io.opos, &FastLookUpTable[x * 1000], 1);
+        memcpy(io.opos, &FastIODigitTable[x * 1000], 1);
         io.opos += 1;
     }
 }
 
 template <std::unsigned_integral T>
 inline void FastWrite(FASTIO& io, T x) noexcept {
-    if (x > 9999'9999'9999'9999) {
+    if (9999'9999'9999'9999 < x) {
         FastWrite__(io, x / 10000'0000'0000'0000);
         FastWrite_(io, x / 10000'0000'0000 % 10000);
         FastWrite_(io, x / 10000'0000 % 10000);
         FastWrite_(io, x / 10000 % 10000);
         FastWrite_(io, x % 10000);
-    } else if (x > 9999'9999'9999) {
+    } else if (9999'9999'9999 < x) {
         FastWrite__(io, x / 10000'0000'0000);
         FastWrite_(io, x / 10000'0000 % 10000);
         FastWrite_(io, x / 10000 % 10000);
         FastWrite_(io, x % 10000);
-    } else if (x > 9999'9999) {
+    } else if (9999'9999 < x) {
         FastWrite__(io, x / 10000'0000);
         FastWrite_(io, x / 10000 % 10000);
         FastWrite_(io, x % 10000);
-    } else if (x > 9999) {
+    } else if (9999 < x) {
         FastWrite__(io, x / 10000);
         FastWrite_(io, x % 10000);
     } else {
