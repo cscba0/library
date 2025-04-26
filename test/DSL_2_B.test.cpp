e0991fca2c3d74c9de038626ac329833c0458fa5
cscba0 <cscba01367@gmail.com>
2025-04-26 20:03:19 +0900

    add(test): dynamic segment tree

---
+++ test/DSL_2_B.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/all/DSL_2_B"
+
+#include <data_structure/dynamic_segment_tree.hpp>  // for DynamicSegmentTree
+#include <fastio/base.hpp>                          // for FASTIO, cin, cout
+#include <fastio/char/write.hpp>                    // for operator<<
+#include <fastio/signed/read.hpp>                   // for operator>>
+#include <fastio/signed/write.hpp>                  // for operator<<
+#include <templates/macro/abbrev/endl.hpp>          // for endl
+#include <templates/macro/segtree/RSQ.hpp>          // for RSQ
+#include <templates/qcfium.hpp>
+#include <templates/rep.hpp>                        // for rep
+#include <version>                                  // for std
+
+using namespace std;
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    DynamicSegmentTree<RSQ(int, 0)> seg;
+    rep(_, q) {
+        int T, x, y;
+        cin >> T >> x >> y;
+        if (T) {
+            cout << seg(x, y + 1) << endl;
+        } else {
+            seg.set(x, y);
+        }
+    }
+}
