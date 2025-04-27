4f85547af03af4c499cefc3abeedb4066321d3f7
cscba0 <cscba01367@gmail.com>
2025-04-27 15:14:11 +0900

    update(dynamic_segment_tree): rewrite complete

--- data_structure/dynamic_segment_tree.hpp
+++ data_structure/dynamic_segment_tree.hpp
@@ -24,7 +24,7 @@ struct DynamicSegmentTree {
         S l = 0, r = n;
         bool flg = true;
         while (*ptr) {
-            st.push(ptr);
+            st.emplace(ptr);
             nptr& cur = *ptr;
             S mid = (l + r) >> 1;
             if (cur->p == p) {
@@ -63,7 +63,7 @@ struct DynamicSegmentTree {
         S l = 0, r = n;
         bool flg = true;
         while (*ptr) {
-            st.push(ptr);
+            st.emplace(ptr);
             nptr& cur = *ptr;
             S mid = (l + r) >> 1;
             if (cur->p == p) {
@@ -116,61 +116,88 @@ struct DynamicSegmentTree {
         return e();
     }
 
-    T operator()(int64_t L, int64_t R) {
-        return prod(root, 0, n, L, R);
-    }
-    T prod(const nptr& ptr, int64_t l, int64_t r, int64_t L, int64_t R) {
-        if (!ptr or r <= L or R <= l) return e();
-        if (L <= l and r <= R) return ptr->prod;
-        int64_t mid = (l + r) >> 1;
-        T res = prod(ptr->left, l, mid, L, R);
-        if (L <= ptr->p and ptr->p < R) res = op(res, ptr->v);
-        return op(res, prod(ptr->right, mid, r, L, R));
-    }
-
-    int64_t max_right(int64_t L, const auto& f) {
-        T sum = e();
-        return max_right(root, 0, n, L, sum, f);
-    }
-    int64_t max_right(const nptr& ptr, int64_t l, int64_t r, int64_t L, T& sum, const auto& f) {
-        if (!ptr or r <= L) return n;
-        if (L <= l and f(op(sum, ptr->prod))) {
-            sum = op(sum, ptr->prod);
-            return n;
+    T operator()(S L, S R) {
+        if (!root) return e();
+        T res = e();
+        std::stack<std::tuple<const nptr&, S, S>> st;
+        st.emplace(root, 0, n);
+        while (!st.empty()) {
+            auto [ptr, l, r] = st.top();
+            st.pop();
+            if (!ptr || r <= L || R <= l) continue;
+            if (L <= l && r <= R) {
+                res = op(res, ptr->prod);
+                continue;
+            }
+            S mid = (l + r) >> 1;
+            if (ptr->p >= L && ptr->p < R) {
+                res = op(res, ptr->v);
+            }
+            st.emplace(ptr->right, mid, r);
+            st.emplace(ptr->left, l, mid);
         }
-        int64_t mid = (l + r) >> 1;
-        if (ptr->p < L) {
-            return max_right(ptr->right, mid, r, L, sum, f);
-        }
-        int64_t res = max_right(ptr->left, l, mid, L, sum, f);
-        if (res != n) return res;
-        sum = op(sum, ptr->v);
-        if (!f(sum)) {
-            return ptr->p;
-        }
-        return max_right(ptr->right, mid, r, L, sum, f);
+        return res;
     }
 
-    int64_t min_left(int64_t R, const auto& f) {
+    S max_right(S L, const auto& f) {
         T sum = e();
-        return min_left(root, 0, n, R, sum, f);
+        std::stack<std::tuple<const nptr&, S, S, bool>> st;
+        st.emplace(root, 0, n, true);
+        while (!st.empty()) {
+            auto [ptr, l, r, flg] = st.top();
+            st.pop();
+            S mid = (l + r) >> 1;
+            if (flg) {
+                if (!ptr || r <= L) continue;
+                if (L <= l && f(op(sum, ptr->prod))) {
+                    sum = op(sum, ptr->prod);
+                    continue;
+                }
+                if (ptr->p < L) {
+                    st.emplace(ptr->right, mid, r, true);
+                } else {
+                    st.emplace(ptr, l, r, false);
+                    st.emplace(ptr->left, l, mid, true);
+                }
+            } else {
+                sum = op(sum, ptr->v);
+                if (!f(sum)) {
+                    return ptr->p;
+                }
+                st.emplace(ptr->right, mid, r, true);
+            }
+        }
+        return n;
     }
-    int64_t min_left(const nptr& ptr, int64_t l, int64_t r, int64_t R, T& sum, const auto& f) {
-        if (!ptr or R <= l) return 0;
-        if (r <= R and f(op(ptr->prod, sum))) {
-            sum = op(ptr->prod, sum);
-            return 0;
+
+    S min_left(S R, const auto& f) {
+        T sum = e();
+        std::stack<std::tuple<const nptr&, S, S, bool>> st;
+        st.emplace(root, 0, n, true);
+        while (!st.empty()) {
+            auto [ptr, l, r, first] = st.top();
+            st.pop();
+            if (!ptr || R <= l) continue;
+            S mid = (l + r) >> 1;
+            if (first) {
+                if (r <= R && f(op(ptr->prod, sum))) {
+                    sum = op(ptr->prod, sum);
+                    continue;
+                }
+                if (R <= ptr->p) {
+                    st.emplace(ptr->left, l, mid, true);
+                } else {
+                    st.emplace(ptr, l, r, false);
+                    st.emplace(ptr->right, mid, r, true);
+                }
+            } else {
+                sum = op(ptr->v, sum);
+                if (!f(sum)) {
+                    return ptr->p + 1;
+                }
+                st.emplace(ptr->left, l, mid, true);
+            }
         }
-        int64_t mid = (l + r) >> 1;
-        if (R <= ptr->p) {
-            return min_left(ptr->left, l, mid, R, sum, f);
-        }
-        int64_t res = min_left(ptr->right, mid, r, R, sum, f);
-        if (res != 0) return res;
-        sum = op(ptr->v, sum);
-        if (!f(sum)) {
-            return ptr->p + 1;
-        }
-        return min_left(ptr->left, l, mid, R, sum, f);
+        return 0;
     }
 };
