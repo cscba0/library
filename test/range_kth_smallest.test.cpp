f1f9549608f900563009385cdd5621ac84154989
cscba0 <cscba01367@gmail.com>
2025-04-29 12:58:36 +0900

    add(test): range_kth_smallest

---
+++ test/range_kth_smallest.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_kth_smallest"
+
+#include <algorithm>                                // for max_element
+#include <data_structure/wavelet_matrix/base.hpp>   // for WaveletMatrix
+#include <data_structure/wavelet_matrix/small.hpp>  // for WaveletMatrix::small
+#include <fastio/base.hpp>                          // for FASTIO, cin, cout
+#include <fastio/char/write.hpp>                    // for operator<<
+#include <fastio/signed/read.hpp>                   // for operator>>
+#include <fastio/signed/write.hpp>                  // for operator<<
+#include <fastio/vector/read.hpp>                   // for operator>>
+#include <templates/macro/abbrev/endl.hpp>          // for endl
+#include <templates/qcfium.hpp>
+#include <templates/rep.hpp>                        // for rep
+#include <vector>                                   // for vector
+
+using namespace std;
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    cin >> a;
+    WaveletMatrix<int> wm(a);
+    rep(_, q) {
+        int l, r, k;
+        cin >> l >> r >> k;
+        cout << wm.small(l, r, k) << endl;
+    }
+}
