8939ba6b8f31afa2d068ebbb72069de786297faf
cscba0 <cscba01367@gmail.com>
2025-05-02 21:36:28 +0900

    merge: all includes

--- all.hpp
+++ all.hpp
@@ -1,6 +1,9 @@
 #pragma once
+#include <bits/stdc++.h>
+
 #include <data_structure/all.hpp>
 #include <fastio/all.hpp>
 #include <math/all.hpp>
 #include <misc/all.hpp>
 #include <string/all.hpp>
+#include <templates/all.hpp>
---
+++ experimentael.hpp
@@ -0,0 +1,2 @@
+#pragma once
+#include <data_structure/randomized_binary_search_tree.hpp>
