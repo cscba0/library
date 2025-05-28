57b9e884ee5dc61840b813803e10b535e6e1147e
cscba0 <cscba01367@gmail.com>
2025-05-28 17:05:26 +0900

    update: ranges

--- templates/ranges.hpp
+++ templates/ranges.hpp
@@ -1,45 +1,21 @@
 #pragma once
-#include <algorithm>
 #include <ranges>
 
-namespace _ranges {
-
-template <typename Container>
-struct to_t {
-    template <std::ranges::input_range R>
-    Container operator()(R&& r) const {
-        Container result;
-        std::ranges::copy(r, std::inserter(result, result.end()));
-        return result;
+template <typename F>
+struct pipe_fn {
+    F f;
+    template <typename R>
+    decltype(auto) operator()(R&& r) const {
+        return f(std::forward<R>(r));
     }
 };
 
-template <template <typename...> class Template>
-struct to_template_t {
-    template <std::ranges::input_range R>
-    auto operator()(R&& r) const {
-        using T = std::ranges::range_value_t<R>;
-        return to_t<Template<T>>{}(std::forward<R>(r));
-    }
-};
-
-template <std::ranges::input_range R, typename Container>
-auto operator|(R&& r, const to_t<Container>& f) {
-    return f(std::forward<R>(r));
+template <std::ranges::input_range R, typename F>
+decltype(auto) operator|(R&& r, const pipe_fn<F>& pf) {
+    return pf(std::forward<R>(r));
 }
 
-template <std::ranges::input_range R, template <typename...> class Template>
-auto operator|(R&& r, const to_template_t<Template>& f) {
-    return f(std::forward<R>(r));
+template <typename F>
+constexpr auto pipe(F&& f) {
+    return pipe_fn<std::decay_t<F>>{std::forward<F>(f)};
 }
-
-template <typename Container>
-constexpr auto to() {
-    return to_t<Container>{};
-}
-
-template <template <typename...> class Template>
-constexpr auto to() {
-    return to_template_t<Template>{};
-}
-}  // namespace _ranges
