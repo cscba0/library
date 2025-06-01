fc31e97840833be7324c941050d2650963e77827
cscba0 <cscba01367@gmail.com>
2025-06-01 16:26:58 +0900

    update: RAmQ

--- templates/macro/segtree/RAmQ.hpp
+++ templates/macro/segtree/RAmQ.hpp
@@ -1,8 +1,8 @@
 #pragma once
-#define RAmQ ll,                                       \
-             [](ll a, ll b) { return min(a, b); },     \
-             []() { return 0; },                       \
-             ll,                                       \
-             [](ll lz, ll v) { return lz + v; },       \
-             [](ll cur, ll old) { return cur + old; }, \
-             []() { return 0ll; }
+#define RAmQ(T, E, S, I) T,                                           \
+                         [](T a, T b) -> T { return min(a, b); },     \
+                         []() -> T { return E; },                     \
+                         S,                                           \
+                         [](S lz, T v) -> T { return lz + v; },       \
+                         [](S cur, S old) -> S { return cur + old; }, \
+                         []() -> S { return I; }
